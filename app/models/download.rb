11e94ddf2f9223b79e70b3b806e53fc8312fdb26
Alex Antonov <kaburbundokel11g@inbox.ru>
2014-08-05 16:27:41 +0400

    Video name & service name

--- app/models/download.rb
+++ app/models/download.rb
@@ -3,4 +3,11 @@ class Download < ActiveRecord::Base
 
   mount_uploader :video_file, VideoUploader
   mount_uploader :audio_file, AudioUploader
+
+  before_save do
+    self.original_name = ViddlRb.get_names(self.original_uri).last.split('.')[0..-2].join
+
+    self.service_name = ViddlRb::PluginBase
+      .registered_plugins.find { |p| p.matches_provider?(original_uri) }.to_s.split('::').last
+  end
 end
--- spec/models/download_spec.rb
+++ spec/models/download_spec.rb
@@ -18,4 +18,11 @@ RSpec.describe Download, type: :model do
       expect(Fabricate.build(:download, original_uri: invalid_video_uri)).to be_invalid
     end
   end
+
+  it 'set right filename' do
+    download = Fabricate(:download)
+
+    expect(download.original_name).to eq('Кот красиво прыгнул')
+    expect(download.service_name).to eq('Youtube')
+  end
 end
